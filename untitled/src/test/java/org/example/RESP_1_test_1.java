db3881d735c6dae6e73346d20e8da539fdba19c1
WilsonLi33092 <90936549+WilsonLi33092@users.noreply.github.com>
2024-10-03 22:58:08 -0400

    R-TEST
    RESP-1: Checking to see if adventure and event decks are the correct size.

---
+++ untitled/src/test/java/org/example/RESP_1_test_1.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RESP_1_test_1 {
+    @Test
+    void getAdventureDeckSize() {
+        Main game = new Main();
+        game.intializeAdventureDeck();
+        int decksize = game.getAdventureDeckSize();
+        assertEquals(100,decksize);
+    }
+    @Test
+    void getEventDeckSize() {
+        Main game = new Main();
+        game.intializeEventDeck();
+        int decksize = game.getEventDeckSize();
+        assertEquals(100,decksize);
+    }
+}
