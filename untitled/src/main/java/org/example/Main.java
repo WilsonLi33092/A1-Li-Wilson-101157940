7fde674ee6fbca681718d45a9978cca66317263e
WilsonLi33092 <90936549+WilsonLi33092@users.noreply.github.com>
2024-10-03 22:50:57 -0400

    Revert "RESP-1-test-1"
    This reverts commit 8540dff17f75b53b246dc6480ac85223f5d06577.

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -4,26 +4,14 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Main game = new Main();
-        game.intializeAdventureDeck();
-        game.intializeEventDeck();
-    }
-    class Card {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
 
-    }
-    class questCard {
-
-    }
-    public void intializeAdventureDeck() {
-
-    }
-    public void intializeEventDeck() {
-
-    }
-    public int getAdventureDeckSize() {
-        return 0;
-    }
-    public int getEventDeckSize() {
-        return 0;
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
     }
 }
