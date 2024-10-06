b669d4b146b3a2ce301835085c84aa7b68fe03ac
WilsonLi33092 <90936549+WilsonLi33092@users.noreply.github.com>
2024-10-06 19:45:48 -0400

    R-TEST
    Resp5: checks to see if drawing the queen's favor card adds two cards to the player's hand

--- untitled/src/main/java/org/example/EventCard.java
+++ untitled/src/main/java/org/example/EventCard.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class EventCard {
     public int drawPlagueCard(int playerShields) {
         if(playerShields <= 2) {
@@ -8,5 +10,8 @@ public class EventCard {
         else {
             return playerShields-2;
         }
+    }
+    public ArrayList<Card> drawQueensFavorCard(ArrayList<Card> playerHand, ArrayList<Card> adventureDeck) {
+        return playerHand;
     }
 }
---
+++ untitled/src/test/java/org/example/RESP_5_Test_1.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class RESP_5_Test_1 {
+    @Test
+    public void testQueenFavor() {
+      Decks deck = new Decks();
+      EventCard event = new EventCard();
+      deck.intializeEventDeck();
+      deck.intializeAdventureDeck();
+      deck.dealPlayersHands();
+      deck.getCurrentPlayerTurn();
+      deck.drawEventCard();
+      Player playerOne = new Player();
+      Player playerTwo = new Player();
+      Player playerThree = new Player();
+      Player playerFour = new Player();
+      playerOne.setHand(deck.player1Hand);
+      playerTwo.setHand(deck.player2Hand);
+      playerThree.setHand(deck.player3Hand);
+      playerFour.setHand(deck.player4Hand);
+      int currentHandSize = 0;
+      int revisedHandSize = 0;
+      if(deck.getCurrentPlayerTurn() == 1) {
+          currentHandSize = playerOne.getHand().size();
+          playerOne.setHand(event.drawQueensFavorCard(playerOne.getHand(), deck.adventureDeck));
+          revisedHandSize = playerOne.getHand().size();
+      }
+      else if(deck.getCurrentPlayerTurn() == 2) {
+          currentHandSize = playerTwo.getHand().size();
+          playerTwo.setHand(event.drawQueensFavorCard(playerTwo.getHand(), deck.adventureDeck));
+          revisedHandSize = playerTwo.getHand().size();
+      }
+      else if(deck.getCurrentPlayerTurn() == 3) {
+          currentHandSize = playerThree.getHand().size();
+          playerThree.setHand(event.drawQueensFavorCard(playerThree.getHand(), deck.adventureDeck));
+          revisedHandSize = playerThree.getHand().size();
+      }
+      else if(deck.getCurrentPlayerTurn() == 4) {
+          currentHandSize = playerFour.getHand().size();
+          playerFour.setHand(event.drawQueensFavorCard(playerFour.getHand(), deck.adventureDeck));
+          revisedHandSize = playerFour.getHand().size();
+      }
+      assertEquals((currentHandSize + 2), revisedHandSize);
+
+    }
+}
