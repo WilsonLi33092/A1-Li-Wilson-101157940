042a164afd71f7e0412beceab8ef82a83d91e79c
WilsonLi33092 <90936549+WilsonLi33092@users.noreply.github.com>
2024-10-06 18:58:23 -0400

    R-TEST
    RESP4: checks if plague card correctly takes away two shields from the player

--- untitled/src/test/java/org/example/RESP_4_Test_1.java
+++ untitled/src/test/java/org/example/RESP_4_Test_1.java
@@ -1,7 +1,50 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RESP_4_Test_1 {
-    
+    @Test
+    public void testPlague() {
+        Decks deck = new Decks();
+        EventCard event = new EventCard();
+        deck.intializeEventDeck();
+        deck.intializeAdventureDeck();
+        deck.dealPlayersHands();
+        deck.getCurrentPlayerTurn();
+        deck.drawEventCard();
+        Player playerOne = new Player();
+        Player playerTwo = new Player();
+        Player playerThree = new Player();
+        Player playerFour = new Player();
+        int currentShields = -1;
+        int revisedShields = -1;
+        if(deck.getCurrentPlayerTurn() == 1) {
+            currentShields = playerOne.getShields();
+            playerOne.setShields(event.drawPlagueCard(playerOne.getShields()));
+            revisedShields = playerOne.getShields();
+        }
+        else if(deck.getCurrentPlayerTurn() == 2) {
+            currentShields = playerTwo.getShields();
+            playerTwo.setShields(event.drawPlagueCard(playerTwo.getShields()));
+            revisedShields = playerTwo.getShields();
+        }
+        else if(deck.getCurrentPlayerTurn() == 3) {
+            currentShields = playerThree.getShields();
+            playerThree.setShields(event.drawPlagueCard(playerThree.getShields()));
+            revisedShields = playerThree.getShields();
+        }
+        else if(deck.getCurrentPlayerTurn() == 4) {
+            currentShields = playerFour.getShields();
+            playerFour.setShields(event.drawPlagueCard(playerFour.getShields()));
+            revisedShields = playerFour.getShields();
+        }
+        if(currentShields <= 2 ) {
+            assertEquals(0, revisedShields);
+        }
+        else {
+            assertEquals((currentShields-2), revisedShields);
+        }
+    }
 }
