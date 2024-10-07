2972ce0d17bdaad2aa42e2619fd0300ac99ce5d5
WilsonLi33092 <90936549+WilsonLi33092@users.noreply.github.com>
2024-10-06 23:37:09 -0400

    R-TEST
    resp7: test checks to see if deleting card correctly deletes a card from the player's hand and changes the size

---
+++ untitled/src/test/java/org/example/RESP_7_Test_1.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+class RESP_7_Test_1 {
+    private final ByteArrayOutputStream outputStreamCap = new ByteArrayOutputStream();
+    private PrintStream oringinalOutput;
+    private Player playerOne;
+    private Player playerTwo;
+    private Player playerThree;
+    private Player playerFour;
+    private Decks deck;
+    @BeforeEach
+    public void setUp(){
+        deck = new Decks();
+        EventCard event = new EventCard();
+        deck.intializeEventDeck();
+        deck.intializeAdventureDeck();
+        deck.dealPlayersHands();
+        deck.drawEventCard();
+        playerOne = new Player();
+        playerTwo = new Player();
+        playerThree = new Player();
+        playerFour = new Player();
+        playerOne.setHand(deck.player1Hand);
+        playerTwo.setHand(deck.player2Hand);
+        playerThree.setHand(deck.player3Hand);
+        playerFour.setHand(deck.player4Hand);
+    }
+    @Test
+    public void testEventDisplay() {
+        assertFalse(playerOne.getHand().isEmpty());
+        int playerHandSizeBeforeDeletion = playerOne.hand.size();
+        playerOne.deleteCard(0);
+        assertEquals((playerHandSizeBeforeDeletion-1), deck.player1Hand.size());
+
+    }
+    @AfterEach
+    public void tearDown() {
+        System.setOut(oringinalOutput);
+    }
+}
