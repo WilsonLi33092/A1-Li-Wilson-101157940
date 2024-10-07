0595c5592296d7ebbc59b2c7f103a4239b37190f
WilsonLi33092 <90936549+WilsonLi33092@users.noreply.github.com>
2024-10-07 00:44:16 -0400

    R-CODE
    Resp8: code that makes it so when the prosperity card is drawn each player adds two cards to their hand and the adventure deck size is also updated

--- untitled/src/main/java/org/example/EventCard.java
+++ untitled/src/main/java/org/example/EventCard.java
@@ -23,6 +23,26 @@ public class EventCard {
         return playerHand;
     }
     public void drawProsperity(ArrayList<Card> playerOneHand, ArrayList<Card> playerTwoHand, ArrayList<Card> playerThreeHand, ArrayList<Card> playerFourHand, ArrayList<Card> adventureDeck) {
-
+        Random drawCard = new Random();
+        for(int i =0; i<8;i++) {
+            int randInx = drawCard.nextInt(adventureDeck.size());
+            Card randCard = adventureDeck.get(randInx);
+            if(i % 4 == 0) {
+                playerOneHand.add(randCard);
+                adventureDeck.remove(randInx);
+            }
+            else if(i % 4 == 1) {
+                playerTwoHand.add(randCard);
+                adventureDeck.remove(randInx);
+            }
+            else if(i % 4 == 3) {
+                playerThreeHand.add(randCard);
+                adventureDeck.remove(randInx);
+            }
+            else {
+                playerFourHand.add(randCard);
+                adventureDeck.remove(randInx);
+            }
+        }
     }
 }
