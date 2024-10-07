d6af0033251833339cceec641efe194ccdc5f919
WilsonLi33092 <90936549+WilsonLi33092@users.noreply.github.com>
2024-10-07 15:35:35 -0400

    R-CODE
    Resp9: game goes through and simulates where every player declines to sponsor the quest that was drawn and the turn goes to the next player

--- untitled/src/main/java/org/example/QuestCard.java
+++ untitled/src/main/java/org/example/QuestCard.java
@@ -15,6 +15,32 @@ class QuestCard {
         return type + " " + value;
     }
     public int questCardDrawn(int currentPlayerTurn, List<String> playerDecisions){
-        return 0;
+        boolean questDeclined = true;
+        int startingPlayer = currentPlayerTurn;
+        int decisionIndex = 0;
+
+        while (questDeclined && decisionIndex < playerDecisions.size()) {
+            String player = Integer.toString(currentPlayerTurn);
+            String option = playerDecisions.get(decisionIndex).toUpperCase().trim();
+            decisionIndex++;
+
+            if (option.equals("Y")) {
+                questDeclined = false;
+                System.out.println("Player " + player + " will sponsor the quest.");
+                return currentPlayerTurn;
+            } else if (option.equals("N")) {
+                currentPlayerTurn++;
+                if (currentPlayerTurn == 5) {
+                    currentPlayerTurn = 1;
+                }
+
+                if (currentPlayerTurn == startingPlayer) {
+                    currentPlayerTurn++;
+                    return currentPlayerTurn;
+                }
+            }
+        }
+
+        return currentPlayerTurn;
     }
 }
