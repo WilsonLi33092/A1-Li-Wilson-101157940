3519958763b14230f3ace33e2ed98c3e3c45c703
WilsonLi33092 <90936549+WilsonLi33092@users.noreply.github.com>
2024-10-07 15:08:55 -0400

    R-TEST
    RESP9: test to see if a quest card is drawn and if all player's decline then it skips to the next player's turn

--- untitled/src/main/java/org/example/QuestCard.java
+++ untitled/src/main/java/org/example/QuestCard.java
@@ -12,4 +12,7 @@ class QuestCard {
     public String toString(){
         return type + " " + value;
     }
+    public int questCardDrawn(int currentPLayerTurn) {
+        return 0;
+    }
 }
---
+++ untitled/src/test/java/org/example/RESP_9_Test_1.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+class RESP_9_Test_1 {
+  @Test
+    public void testDeclineQuest() {
+      Decks deck = new Decks();
+      QuestCard quest = new QuestCard();
+      deck.intializeEventDeck();
+      deck.intializeAdventureDeck();
+      deck.dealPlayersHands();
+      deck.drawEventCard();
+      int currentPlayerTurn = deck.getCurrentPlayerTurn();
+      List<String> decisions = Arrays.asList("N", "N", "N", "N");
+      int revisedPlayerTurn = quest.questCardDrawn(currentPlayerTurn);
+      if(currentPlayerTurn < 4) {
+          assertEquals(currentPlayerTurn + 1, revisedPlayerTurn);
+      }
+      else {
+          assertEquals(1, revisedPlayerTurn);
+      }
+  }
+}
