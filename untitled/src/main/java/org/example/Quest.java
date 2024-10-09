3cc5d358baee9fb64aeb484f268cdf512c9070d8
WilsonLi33092 <90936549+WilsonLi33092@users.noreply.github.com>
2024-10-08 23:10:41 -0400

    R-CODE
    Resp11: code to make sure correct partipants withdrawn and/or continue and the stages

--- untitled/src/main/java/org/example/Quest.java
+++ untitled/src/main/java/org/example/Quest.java
@@ -23,12 +23,26 @@ public class Quest {
         return stages;
     }
     public List<Player> getEligibleParticipants(int stageIndex) {
-        return null;
+        List<Player> eligibleParticipants = new ArrayList<>();
+        for(Player player : participants) {
+            if(!player.equals(sponsor) && !withdrawnParticipants.contains(player)) {
+                eligibleParticipants.add(player);
+            }
+        }
+        return eligibleParticipants;
     }
     public void promptParticipation(int stageIndex, Player player, String action) {
-
+        if (action.equalsIgnoreCase("withdraw")) {
+            withdraw(player);
+            System.out.println(player.numPlayer + " has withdrawn from the quest.");
+        } else {
+            System.out.println(player.numPlayer + " is continuing to tackle the current stage.");
+        }
     }
     public void withdraw(Player player) {
+        if(!withdrawnParticipants.contains(player)) {
+            withdrawnParticipants.add(player);
+        }
 
     }
 }
