661ee04caf6df1353e8e0c9d0998f2cb9fcb2bbe
WilsonLi33092 <90936549+WilsonLi33092@users.noreply.github.com>
2024-12-05 23:32:29 -0500

    Rigged Tests Complete
    Completed the rigged Test for the JP deck scenario

--- frontend/test.js
+++ frontend/test.js
@@ -658,12 +658,200 @@ await inputField.sendKeys("yes");
         await driver.quit();
     }
 }
+async function runRiggedJPDeck() {
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    try {
+        await driver.get('http://127.0.0.1:8081');
+        await driver.sleep(1000);
+        let startButton = await driver.findElement(By.xpath("//button[contains(text(), 'RiggedJPDeck')]"));
+        await startButton.click();
+//        await driver.sleep(1000);
+        let drawButton = await driver.findElement(By.xpath("//button[contains(text(), 'RiggedDrawCard')]"));
+        await drawButton.click();
+//        await driver.sleep(1000);
+        let inputField = await driver.findElement(By.xpath("//input[@placeholder='Enter command here...']"));
+        await inputField.sendKeys("no");
+//        await driver.sleep(1000);
+        let submitButton = await driver.findElement(By.xpath("//button[contains(text(), 'Submit')]"));
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("yes");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("0,7");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("2,6");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("3,1,9");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("4,10");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("continue");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("0");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("4,5");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("continue");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("0");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("4,3");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("continue");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("0");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("3,6");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("continue");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("6,5");
+//        await driver.sleep(1000);
+        await submitButton.click();
+        console.log("JP Deck: ")
+        let playerOneHand = await driver.findElement(By.id('player-one-hand')).getText();
+                                if(playerOneHand.includes("Foe 5, Foe 10, Foe 15, Foe 15, Foe 30, Horse 10, Battle-Axe 15, Battle-Axe 15, Lance 20")) {
+                                    console.log("Player One Hand Result Passed");
+                                }
+                                if(playerOneHand.includes("Shields: 0")){
+                                    console.log("Player One Shields Result Passed");
+                                }
+//        await driver.sleep(1000);
+        await inputField.sendKeys("continue");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("8,3");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("continue");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("5,6");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+await inputField.sendKeys("continue");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("8,5,3");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+await inputField.sendKeys("continue");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("6,4,7");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+await inputField.sendKeys("continue");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("6,5,7");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+await inputField.sendKeys("continue");
+//        await driver.sleep(1000);
+        await submitButton.click();
+//        await driver.sleep(1000);
+        await inputField.sendKeys("3,4,5,7");
+//        await driver.sleep(1000);
+        await submitButton.click();
+        await driver.sleep(1000);
+        let winners = await driver.findElement(By.id('winners')).getAttribute('value');
+                        let playerThreeHand = await driver.findElement(By.id('player-three-hand')).getText();
+                        let playerFourHand = await driver.findElement(By.id('player-four-hand')).getText();
+                        if(playerThreeHand.includes("Foe 5, Foe 5, Foe 15, Foe 30, Sword 10")) {
+                            console.log("Player Three Hand Result Passed");
+                        }
+                        if(playerThreeHand.includes("Shields: 0")) {
+                            console.log("Player Three Shields Result Passed");
+                        }
+                        if(playerFourHand.includes("Foe 15, Foe 15, Foe 40, Lance 20")){
+                            console.log("Player Four Hand Result Passed");
+                        }
+                        if(playerFourHand.includes("Shields: 4")) {
+                            console.log("Player Four Shields Result Passed");
+                        }
+                        if(!winners.includes("Player 3") && !winners.includes("Player 1")&& winners.includes("Player 2") && winners.includes("Player 4")){
+                            console.log("Winners Result Passed : \n Player 1 Non-winner Passed \n Player 2 winner passed \n Player 3 Non-winner passed \n Player 4 winner passed");
+                        }
+        await inputField.sendKeys("0");
+        //        await driver.sleep(1000);
+                await submitButton.click();
+        //        await driver.sleep(1000);
+        await inputField.sendKeys("0");
+                //        await driver.sleep(1000);
+                        await submitButton.click();
+                //        await driver.sleep(1000);
+        await inputField.sendKeys("0");
+                //        await driver.sleep(1000);
+                        await submitButton.click();
+                //        await driver.sleep(1000);
+        await inputField.sendKeys("0");
+                //        await driver.sleep(1000);
+                        await submitButton.click();
+                //        await driver.sleep(1000);
+        await driver.sleep(10000);
+        let playerTwoHand = await driver.findElement(By.id('player-two-hand')).getText();
+        let playerTwoHandCards = playerTwoHand.split(" | Shields:");
+        let playerTwoHandCardsFinal = playerTwoHandCards[0];
+        let playerTwoHandSize = playerTwoHandCardsFinal.split(", ");
+        let numCards = playerTwoHandSize.length;
+        if(numCards === 12) {
+        console.log("Player 2 Hand Size Result Passed")
+        }
+    } catch (error) {
+        console.error("Test encountered an error:", error);
+    } finally {
+        await driver.quit();
+    }
+}
 async function runAllTests() {
     let driver = await new Builder().forBrowser('chrome').build();
     try {
-        await runRiggedDeck2Winner(driver);
-        await runRiggedDeck1Winner(driver);
-        await runRiggedDeck0Winner(driver);
+        //await runRiggedDeck2Winner(driver);
+        //await runRiggedDeck1Winner(driver);
+        //await runRiggedDeck0Winner(driver);
+        await runRiggedJPDeck(driver);
     } catch (error) {
         console.error("Error running tests:", error);
     } finally {
